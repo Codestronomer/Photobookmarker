619378f4561b6f5ee3981737323f0272be3437eb
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2020-12-30 01:01:49 +0100

    added actions to activity stream and created display view

--- account/views.py
+++ account/views.py
@@ -9,6 +9,8 @@ from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from common.decorators import ajax_required
+from actions.utils import create_action
+from actions.models import Action
 
 
 # Create your views here.
@@ -38,10 +40,19 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
+    # Displays all actions by default
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list('id',
+                                                       flat=True)
+    if following_ids:
+        # if user is following others, retrieve only their actions
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
     return render(request,
                   'account/dashboard.html',
                   {
-                      'section': dashboard
+                      'section': dashboard,
+                      'actions':actions
                   })
 
 
@@ -58,6 +69,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             Profile.objects.create(user=new_user)
+            create_action(new_user, 'just joined Bookmark')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -122,10 +134,8 @@ def user_follow(request):
         try:
             user = User.objects.get(id=user_id)
             if action == 'follow':
-                Contact.objects.get_or_create(
-                    user_from=request.user,
-                    user_to=user,
-                )
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+                create_action(request.user, 'is following ', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
